998377447161a5eb1514d5a33a0ffee36394a546
johnbutol <mounierlio@gmail.com>
2016-08-08 18:56:21 +0200

    factorisation

--- MCDEmu.ino
+++ MCDEmu.ino
@@ -95,6 +95,8 @@ const int CdInfoMsgT_34W515=R_34W515_CD_INFO;
 Chrysler CD drive (34W539) Transmit Definitions
 **********************************************/
 #define T_34W539 0xF8
+#define T_34W539_UNKNOWN_1ST_MSG {0xF5,0x01,0x09,0xA0,0x80,0x18}
+#define T_34W539_UNKNOWN_2ND_MSG {0xF4,0x00,0x32,0x05,0x05,0x09,0x01,0x0D}
 #define T_34W539_CD_INFO_RESPONSE {0xF2,0x10,0x00,0x05,0x01}
 #define T_34W539_TRACK_INFO_RESPONSE {T_34W539,0x25,0x09}
 #define T_34W539_1ST_MESSAGE {T_34W539,0x82}
@@ -117,40 +119,81 @@ int statusT_34W539[10]={0,0,0,0,0,0,0,0,0,0};
 /**********************************************
 Chrysler CD drive (34W539) Receive Definitions
 **********************************************/
-#define R_34W539_EJECT 0xE1
-#define R_34W539_STOP 0xE2
-#define R_34W539_PLAY 0xE4
+#define R_34W539_UNKNOWN_1ST_MSG {0x5F,0x01,0x09,0xA0,0x80,0x18}
+#define R_34W539_UNKNOWN_2ND_MSG {0xAD}
+#define R_34W539_ACK 0xDB
+#define R_34W539_EJECT {0xE1}
+#define R_34W539_STOP {0xE2}
+#define R_34W539_PLAY {0xE4}
 #define R_34W539_FAST_FORWARD {0xE6,0x02}
 #define R_34W539_REWIND {0xE7,0x02}
-#define R_34W539_NEXT_TRACK 0xEA
+#define R_34W539_NEXT_TRACK {0xEA}
 #define R_34W539_CURRENT_BACK_TRACK {0xEB,0x01}
 #define R_34W539_PREVIOUS_TRACK {0xEB,0x02}
-#define R_34W539_ERROR_STOP 0xF7
-#define R_34W539_RANDOM_DISABLE 0xF0
-#define R_34W539_RANDOM_ENABLE 0xFA
+#define R_34W539_ERROR_STOP {0xF7}
+#define R_34W539_RANDOM_DISABLE {0xF0}
+#define R_34W539_RANDOM_ENABLE {0xFA}
 #define R_34W539_CD_INFO {0xFC,0x10,0x00}
-#define R_34W539_DIRECTORY_SET_PREVIOUS 0xC5
-#define R_34W539_DIRECTORY_SET_NEXT 0xC6
+#define R_34W539_DIRECTORY_SET(dirN) {0xC3,0x00,0x00,dirN,0x01}
+#define R_34W539_DIRECTORY_SET_PREVIOUS {0xC5}
+#define R_34W539_DIRECTORY_SET_NEXT {0xC6}
 #define R_34W539_METADATA 0xC7
 #define R_34W539_METADATA_DIRECTORY_NAME(dirN) {R_34W539_METADATA,0x01,0x02,0x02,0x00,0x00,dirN,0x01}
 #define R_34W539_METADATA_TRACK_NAME(dirN,trkN) {R_34W539_METADATA,0x01,0x02,0x05,0x00,0x00,dirN,trkN}
 #define R_34W539_METADATA_ARTIST_NAME(dirN,trkN) {R_34W539_METADATA,0x01,0x02,0x04,0x00,0x00,dirN,trkN}
 #define R_34W539_METADATA_FILE_NAME(dirN,trkN) {R_34W539_METADATA,0x01,0x02,0x01,0x00,0x00,dirN,trkN}
-#define R_34W539_OTHER_INFO {0xC8,0x10,0x00}
+#define R_34W539_OTHER_INFO_1 {0xC8,0x10,0x00}
+#define R_34W539_OTHER_INFO_2 {0xC8,0x20,0x01}
 #define R_34W539_UNKNOWN_1 {0xC9,0x01}
+#define R_34W539_DIRECTORY_ROOT 0x01
 #define R_34W539_DIRECTORY_1 0x03
+
 int statusR_34W539[10]={0,0,0,0,0,0,0,0,0,0};
 
 int gCurrentTrack = 0;
 int gCurrentDirecory = 0;
 
-const int CdInfoMsgT_34W539[]=R_34W539_CD_INFO;
-const int PreviousTrkMsgT_34W539[]=R_34W539_PREVIOUS_TRACK;
-const int MetaDirNameMsgT_34W539[]=R_34W539_METADATA_DIRECTORY_NAME(R_34W539_DIRECTORY_1);
-const int MetaArtNameMsgT_34W539[]=R_34W539_METADATA_TRACK_NAME(R_34W539_DIRECTORY_1,1);
-const int MetaTrkNameMsgT_34W539[]=R_34W539_METADATA_ARTIST_NAME(R_34W539_DIRECTORY_1,1);
-const int MetaFileNameMsgT_34W539[]=R_34W539_METADATA_FILE_NAME(R_34W539_DIRECTORY_1,1);
-const int OtherInfoMsgT_34W539[]=R_34W539_OTHER_INFO;
+const unsigned char initT_34W539[]=R_34W539_UNKNOWN_1ST_MSG;
+const unsigned char infoDiscT_34W539[]=R_34W539_CD_INFO;
+const unsigned char previousTrackT_34W539[]=R_34W539_PREVIOUS_TRACK;
+const unsigned char metaDirNameT_34W539[]=R_34W539_METADATA_DIRECTORY_NAME(R_34W539_DIRECTORY_1);
+const unsigned char metaTrackNameT_34W539[]=R_34W539_METADATA_TRACK_NAME(R_34W539_DIRECTORY_1,1);
+const unsigned char metaArtNameT_34W539[]=R_34W539_METADATA_ARTIST_NAME(R_34W539_DIRECTORY_1,1);
+const unsigned char otherInfo1T_34W539[]=R_34W539_OTHER_INFO_1;
+const unsigned char otherInfo2T_34W539[]=R_34W539_OTHER_INFO_2;
+const unsigned char playTrackT_34W539[]=R_34W539_PLAY;
+const unsigned char pauseTrackT_34W539[]=R_34W539_STOP;
+const unsigned char nextTrackT_34W539[]=R_34W539_NEXT_TRACK;
+const unsigned char nextDirectoryT_34W539[]=R_34W539_DIRECTORY_SET_NEXT;
+const unsigned char previousDirectoryT_34W539[]=R_34W539_DIRECTORY_SET_PREVIOUS;
+const unsigned char ejectDiscT_34W539[]=R_34W539_EJECT;
+const unsigned char infoTrackT_34W539[]=R_34W539_OTHER_INFO_2;
+const unsigned char randomEnableT_34W539[]=R_34W539_RANDOM_ENABLE;
+const unsigned char randomDisableT_34W539[]=R_34W539_RANDOM_DISABLE;
+const unsigned char fastForwardT_34W539[]=R_34W539_FAST_FORWARD;
+const unsigned char metaFileNameT_34W539[]=R_34W539_METADATA_FILE_NAME(R_34W539_DIRECTORY_1,1);
+const unsigned char rewindT_34W539[]=R_34W539_REWIND;
+
+const unsigned char sizeofinitT_34W539 = sizeof(initT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofinfoDiscT_34W539 = sizeof(infoDiscT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofpreviousTrackT_34W539 = sizeof(previousTrackT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofmetaDirNameT_34W539 = sizeof(metaDirNameT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofinfoTrackT_34W539 = sizeof(infoTrackT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofejectDiscT_34W539 = sizeof(ejectDiscT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofrandomEnableT_34W539 = sizeof(randomEnableT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofrandomDisableT_34W539 = sizeof(randomDisableT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeoffastForwardT_34W539 = sizeof(fastForwardT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofrewindT_34W539 = sizeof(rewindT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofpreviousDirectoryT_34W539 = sizeof(previousDirectoryT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofnextDirectoryT_34W539 = sizeof(nextDirectoryT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofnextTrackT_34W539 = sizeof(nextTrackT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofpauseTrackT_34W539 = sizeof(pauseTrackT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofmetaFileNameT_34W539 = sizeof(metaFileNameT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofplayTrackT_34W539 = sizeof(playTrackT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofotherInfo2T_34W539 = sizeof(otherInfo2T_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofmetaArtNameT_34W539 = sizeof(metaArtNameT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofmetaTrackNameT_34W539 = sizeof(metaTrackNameT_34W539)/sizeof(const unsigned char);
+const unsigned char sizeofotherInfo1T_34W539 = sizeof(otherInfo1T_34W539)/sizeof(const unsigned char);
 
 /**********************************************
 SETUP
@@ -222,13 +265,14 @@ typedef struct
   bool otherInfoReq;
 }cmdRequest_s;
 
-cmdRequest_s cmdRequest={false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+cmdRequest_s cmdRequest = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+char responseSize = 0;
 
 typedef struct
 {
   char serialCmd;
   bool *cmdRequest;
-  char *printSerialMsg;
+  const char *printSerialMsg;
 }cmdtabRequest_s;
 
 const cmdtabRequest_s tabSerialRequest[]=
@@ -355,198 +399,143 @@ void MCDEmu_Slave_34W515(void)
 /**********************************************
 (3) Generic Layer: connecting the dots
 **********************************************/
+
 void MCDEmu_Generic_Commands(void)
 {
-  int returnValue=0;
 
-  if(cmdRequest.printHelp)
-  {
-    cmdRequest.printHelp=false;
-    printHelp();
-  }
-  if(cmdRequest.initReq)
-  {
-    cmdRequest.initReq=false;
-    returnValue = digitalSWSPIWrite(0x55);
-  }
-  if(cmdRequest.playTrackReq)
-  {
-    cmdRequest.playTrackReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_PLAY);
-  }
-  if(cmdRequest.randomEnableReq)
-  {
-    cmdRequest.randomEnableReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_RANDOM_ENABLE);
-  }
-  if(cmdRequest.randomDisableReq)
-  {
-    cmdRequest.randomDisableReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_RANDOM_DISABLE);
-  }
-  if(cmdRequest.nextDirectoryReq)
-  {
-    cmdRequest.nextDirectoryReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_DIRECTORY_SET_NEXT);
-  }
-  if(cmdRequest.previousDirectoryReq)
-  {
-    cmdRequest.previousDirectoryReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_DIRECTORY_SET_PREVIOUS);
-  }
-  if(cmdRequest.pauseTrackReq)
-  {
-    cmdRequest.pauseTrackReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_STOP);
-  }
-  if(cmdRequest.nextTrackReq)
-  {
-    cmdRequest.nextTrackReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_NEXT_TRACK);
-  }
-  if(cmdRequest.previousTrackReq)
-  {
-    cmdRequest.previousTrackReq=false;
-    returnValue = digitalSWSPIWrite(PreviousTrkMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(PreviousTrkMsgT_34W539[1]);
-  }
-  if(cmdRequest.ejectDiscReq)
-  {
-    cmdRequest.ejectDiscReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_EJECT);
-  }
-  if(cmdRequest.infoDiscReq)
-  {
-    cmdRequest.infoDiscReq=false;
-    returnValue = digitalSWSPIWrite(CdInfoMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(CdInfoMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(CdInfoMsgT_34W539[2]);
-  }
-  if(cmdRequest.infoTrackReq)
-  {
-    cmdRequest.infoTrackReq=false;
-    returnValue = digitalSWSPIWrite(R_34W539_EJECT);
-  }
-  if(cmdRequest.metaDirNameReq)
-  {
-    cmdRequest.metaDirNameReq=false;
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[2]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[3]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[4]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[5]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[6]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaDirNameMsgT_34W539[7]);
-  }
-  if(cmdRequest.metaArtNameReq)
-  {
-    cmdRequest.metaArtNameReq=false;
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[2]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[3]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[4]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[5]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[6]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaArtNameMsgT_34W539[7]);
-  }
-  if(cmdRequest.metaTrackNameReq)
-  {
-    cmdRequest.metaTrackNameReq=false;
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[2]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[3]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[4]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[5]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[6]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaTrkNameMsgT_34W539[7]);
-  }
-  if(cmdRequest.metaFileNameReq)
-  {
-    cmdRequest.metaFileNameReq=false;
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[2]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[3]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[4]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[5]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[6]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(MetaFileNameMsgT_34W539[7]);
-  }
-  if(cmdRequest.otherInfoReq)
-  {
-    cmdRequest.otherInfoReq=false;
-    returnValue = digitalSWSPIWrite(OtherInfoMsgT_34W539[0]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(OtherInfoMsgT_34W539[1]);
-    delay(16);
-    returnValue = digitalSWSPIWrite(OtherInfoMsgT_34W539[2]);
-  }
 }
 
 /**********************************************
 (4) 34W539 Master protocol emulator
 **********************************************/
+
+typedef struct
+{
+  bool *request;
+  const unsigned char *mesg;
+  unsigned char transmitSize;
+  unsigned char responseSize;
+}transmitMsg_s;
+
+const transmitMsg_s transmitMsg[]=
+{
+ //COMMAND LIST                     //MESSAGE TO SEND             //SIZE TO SEND                  //SIZE TO RECEIVE
+  {&cmdRequest.initReq ,             initT_34W539,               sizeofinitT_34W539,               6},
+  {&cmdRequest.ejectDiscReq ,        ejectDiscT_34W539,          sizeofejectDiscT_34W539,          9},
+  {&cmdRequest.pauseTrackReq ,       pauseTrackT_34W539,         sizeofpauseTrackT_34W539,         9},
+  {&cmdRequest.playTrackReq ,        playTrackT_34W539,          sizeofplayTrackT_34W539,          9},
+  {&cmdRequest.nextTrackReq ,        nextTrackT_34W539,          sizeofnextTrackT_34W539,          9},
+  {&cmdRequest.previousTrackReq ,    previousTrackT_34W539,      sizeofpreviousTrackT_34W539,      9},
+  {&cmdRequest.fastForwardReq ,      fastForwardT_34W539,        sizeoffastForwardT_34W539,        9},
+  {&cmdRequest.rewindReq ,           rewindT_34W539,             sizeofrewindT_34W539,             9},  
+  {&cmdRequest.infoDiscReq ,         infoDiscT_34W539,           sizeofinfoDiscT_34W539,           9},
+  {&cmdRequest.infoTrackReq ,        infoTrackT_34W539,          sizeofinfoTrackT_34W539,          9},
+  {&cmdRequest.randomEnableReq ,     randomEnableT_34W539,       sizeofrandomEnableT_34W539,       9},
+  {&cmdRequest.randomDisableReq ,    randomDisableT_34W539,      sizeofrandomDisableT_34W539,      9},
+  {&cmdRequest.metaDirNameReq ,      metaDirNameT_34W539,        sizeofmetaDirNameT_34W539,        25},
+  {&cmdRequest.metaArtNameReq ,      metaArtNameT_34W539,        sizeofmetaArtNameT_34W539,        25},
+  {&cmdRequest.metaTrackNameReq ,    metaTrackNameT_34W539,      sizeofmetaTrackNameT_34W539,      25},
+  {&cmdRequest.metaFileNameReq ,     metaFileNameT_34W539,       sizeofmetaFileNameT_34W539,       25},
+  {&cmdRequest.otherInfoReq ,        otherInfo1T_34W539,         sizeofotherInfo1T_34W539,         9},
+  {&cmdRequest.nextDirectoryReq ,    nextDirectoryT_34W539,      sizeofnextDirectoryT_34W539,      9},
+  {&cmdRequest.previousDirectoryReq, previousDirectoryT_34W539,  sizeofpreviousDirectoryT_34W539,  9},
+  //always last
+  {NULL,NULL,0}
+};
+
+const unsigned char sizeof_transmitMsg = sizeof(transmitMsg)/sizeof(transmitMsg_s);
+
 void MCDEmu_Master_34W539(void)
 {
+  unsigned char returnValue=0;
+  unsigned char i;
+  unsigned char byteIndex;
+  unsigned char tabreturnValue[25]={"\0"};
+  const transmitMsg_s * ltransmitMsg;
 
+  ltransmitMsg = transmitMsg;
+  
+  if(cmdRequest.printHelp)
+  {
+    cmdRequest.printHelp=false;
+    printHelp();
+  }
+  else
+  {
+    for(i=0;i<sizeof_transmitMsg;i++)
+    {
+      if(ltransmitMsg->request != NULL)
+      {
+        if(*ltransmitMsg->request == true)
+        {
+          *ltransmitMsg->request = false;
+          for(byteIndex=0;byteIndex<ltransmitMsg->transmitSize;byteIndex++)
+          {
+            returnValue = digitalSWSPITransfer(ltransmitMsg->mesg[byteIndex]);
+            if(byteIndex != (ltransmitMsg->transmitSize-1))
+            {
+              delay(2);
+            }
+          }
+        }
+      }
+      ltransmitMsg++;
+    }
+  }
+
+  if(!digitalReadFast(_STM_34W539_CS_PIN) && responseSize == 0)
+  {
+ //   responseSize = 6;
+  }
+  i=responseSize;
+  while(responseSize!=0)
+  {
+    responseSize--;
+    returnValue = digitalSWSPITransfer(R_34W539_ACK);
+    if(returnValue < ' ' || returnValue > '}')
+    {
+      Serial.print(returnValue);
+      Serial.print(" ");
+    }
+    else
+    {
+      tabreturnValue[i-responseSize] = returnValue;
+      if(responseSize==0)Serial.print(*tabreturnValue);
+    }
+    if(responseSize==0)Serial.print("\n");
+  }
 }
 
 /**********************************************
 Software SPI Transfer
 **********************************************/
-int digitalSWSPIWrite(int value)
+unsigned char digitalSWSPITransfer(unsigned char valuetosend)
 {
-  int bitPosition;
+  unsigned char bitPosition;
+  unsigned char valuetoreceive = 0;
 
-  while(!digitalReadFast(_STM_34W539_CS_PIN));
-  
+  if(valuetosend != R_34W539_ACK)
+  {
+    while(!digitalReadFast(_STM_34W539_CS_PIN));
+  }
+  else
+  {
+    while(digitalReadFast(_STM_34W539_CS_PIN));
+    delay(4);
+  }
+
   // take the SS pin low to select the chip:
-/*  digitalWriteFast(DMTS_34W539_MOSI_PIN, LOW);
-  delayMicroseconds(1);
-  digitalWriteFast(DMTS_34W539_MOSI_PIN, HIGH);
-  delayMicroseconds(6);
-*/
   digitalWriteFast(_MTS_34W539_CS_PIN, LOW);
 
   for(bitPosition=0;bitPosition<8;bitPosition++)
   {
     while(digitalReadFast(_SCK_34W539_CLK_PIN));
 
-    if(((value & (1<<bitPosition))>>bitPosition) == 0)
+    //READ
+    valuetoreceive |=digitalReadFast(DSTM_34W539_MISO_PIN)<<bitPosition;
+
+    //WRITE
+    if((valuetosend & (1<<bitPosition)) == 0)
     {
         digitalWriteFast(DMTS_34W539_MOSI_PIN, LOW);
     }
@@ -554,6 +543,7 @@ int digitalSWSPIWrite(int value)
     {
         digitalWriteFast(DMTS_34W539_MOSI_PIN, HIGH);
     }
+
     while(!digitalReadFast(_SCK_34W539_CLK_PIN));
   }
 
@@ -563,8 +553,8 @@ int digitalSWSPIWrite(int value)
   
   // take the SS pin high to de-select the chip:
   digitalWriteFast(_MTS_34W539_CS_PIN, HIGH);
-  
-  return 1;
+
+  return valuetoreceive;
 }
 
 /**********************************************
